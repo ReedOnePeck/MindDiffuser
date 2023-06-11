2463933afddb31d31e1b02c930687989e30bfe7c
Yizhuo Lu <96508891+ReedOnePeck@users.noreply.github.com>
2023-06-11 21:42:08 +0800

    Update and rename 筛选奇数层特征.py to Structural_feature_selection.py

--- Feature extractor/Structural_feature_selection.py
+++ Feature extractor/Structural_feature_selection.py
@@ -34,12 +34,12 @@ def CLIP_feature_layer(CLIP_feature_all_layer,layer_name):
 def cal_pccs(X, Y):
 	XMean = np.mean(X)
 	YMean = np.mean(Y)
-	#标准差
+	
 	XSD = np.std(X)
 	YSD = np.std(Y)
-	#z分数
+	
 	ZX = (X-XMean)/(XSD+1e-30)
-	ZY = (Y-YMean)/(YSD+1e-30)#相关系数
+	ZY = (Y-YMean)/(YSD+1e-30)
 	r = np.sum(ZX*ZY)/(len(X))
 	return(r)
 
@@ -49,7 +49,6 @@ x_trn = scaler.fit_transform(x_trn)
 
 
 def feature_select_onefold(k_folder, rate, fold_id, y_trn_all):
-    # 总训练集，在做交叉验证法的时候，验证集来自总训练集，当筛选完特征之后，再用总训练集进行训练
     y_trn = y_trn_all[:, 38400 * (fold_id - 1):38400 * fold_id]
 
     kf = KFold(n_splits=k_folder, shuffle=False)
@@ -73,25 +72,17 @@ def feature_select_onefold(k_folder, rate, fold_id, y_trn_all):
         print("-------------------------------------------------------------------")
 
     cor_mean = np.mean(np.array(cor_k_folder), axis=0)
-    """
-    
-    bar = np.percentile(cor_mean, rate)
-    selected_index = []
-    for i in range(cor_mean.shape[0]):
-        if cor_mean[i] >= bar:
-            selected_index.append(i)
-    # selected_index是feature中k折交叉验证时大于等于%分位数的那些索引，在后续解码过程中，只需要y[:,selected_index]即可，同时在重建时，首先创建一个[1,38400]的全0矩阵，然后按照selected_index的索引把监督信息写入其中
-    """
+
     return cor_mean
 
 
 def feature_select(k_folder=5, layer_name = 'Linear-1',rate = 50):
-    model_save_path = '/nfs/diskstation/DataStation/ChangdeDu/LYZ/图像重建/数据集/解码特征选取/{}/'.format(layer_name)
+    model_save_path = './图像重建/数据集/解码特征选取/{}/'.format(layer_name)
     if not os.path.exists(model_save_path):
         os.makedirs(model_save_path)
 
     trn_CLIP_feature_all_layer = np.concatenate([fetch_CLIP_feature(
-        "/nfs/diskstation/DataStation/ChangdeDu/LYZ/图像重建/数据集/奇数层特征提取1-11/trn_stim_CLIP_zscore_{}.npy".format(
+        "./图像重建/数据集/奇数层特征提取1-11/trn_stim_CLIP_zscore_{}.npy".format(
             i + 1)) for i in range(8)], axis=0)
     y_trn_all = CLIP_feature_layer(trn_CLIP_feature_all_layer, layer_name)
     print('数据加载完毕')
@@ -123,19 +114,6 @@ e = feature_select(k_folder=5, layer_name = 'Linear-7',rate = 97)
 
 
 
-
-"""
-def selecting(layer_names,rate):
-    for name in layer_names:
-        model_save_path = '/nfs/diskstation/DataStation/ChangdeDu/LYZ/图像重建/数据集/解码特征选取/{}/'.format(name)
-        if not os.path.exists(model_save_path):
-            os.makedirs(model_save_path)
-        Linear = np.array(feature_select(k_folder=5, layer_name=name, rate=rate))
-        np.save(model_save_path+'Folder_5_{}.npy'.format(100-rate), Linear)
-    return
-
-a = selecting(layer_names=['Linear-1', 'Linear-3', 'Linear-5', 'Linear-7'],rate=95)
-"""
 
 
 
